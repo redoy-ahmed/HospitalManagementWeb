83d66a192240d0df093aa90c7b07b6edfdf0552f
redoy <redoyahmd@gmail.com>
2017-07-20 21:41:28 +0600

    First Commit

---
+++ HospitalManagementWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HospitalManagementWeb.Startup))]
+namespace HospitalManagementWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
